fc255b41956a28af8a527a42491d02af1e13c188
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2021-10-20 13:53:37 +0530

    Adding code

---
+++ 17_Graphs/11_DFS_Disconnected.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+void printGraph(vector<int> adj[], int V)
+{
+    for (int i = 0; i < V; i++)
+    {
+        for (auto x : adj[i])
+            cout << x << " ";
+
+        cout << endl;
+    }
+}
+void DFS_helper(vector<int> adj[], int s, vector <bool> &visited)
+{
+    visited[s] = true;
+    cout << s << " ";
+    for (auto v : adj[s])
+    {
+        if (visited[v] == false)
+            DFS_helper(adj, v, visited);
+    }
+}
+void DFS(vector<int> adj[], int v)
+{
+    vector <bool> visited(v + 1);
+    for(int i=0;i<v;i++)
+    {
+        if(visited[i]==false)
+        DFS_helper(adj,i,visited);
+    }
+}
+int main()
+{
+    int V = 5;
+    vector<int> adj[V]; // In this implementation we use array of vectors we can also use vector of vectors to allow flexiblity
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 2);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 1);
+    addEdge(adj, 4, 1);
+    addEdge(adj, 3, 1);
+    DFS(adj,V);
+    return 0;
+}
---
+++ 17_Graphs/9_BFSApplications.md
@@ -0,0 +1,19 @@
+# Applications of BFS:
+
+**Shortest path in a unweighted graph**: since BFS finds all the edges connected a vertex hence it also finds the shortest path. Also the shortest path algorithms like Dijkstra's also use the BFS to find the shortest path and instead of using normal queue they use priority queue.
+
+**Crawlers in search engine**: go to pages and then their neighbours and their neighbours.
+
+**Peer to peer networks**
+
+**Social Network Search**: shows us our friends, then their friends and so on.
+
+**In garbage collection**: searches the memory which is reachable by the system a memory is not its deleted
+
+**Cycle detection**: both DPS & BFS are used in cycle detection.
+
+**Ford fulkerson algorithm**: a maximum flow algorithm and BFS provides better time complexity in comparision to DFS.
+
+**Broadcasting**: systems broadkcast to their neighbours which in turn broadcast to their neighbours.
+
+(There are many more applications of BPS and many algorithms based on BFS).
